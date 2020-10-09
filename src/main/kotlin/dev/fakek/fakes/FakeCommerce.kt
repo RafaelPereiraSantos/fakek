d85be6a6cab68fb12eb0406accda076fbde568b7
RafaelPereiraSantos <rafaelps999@gmail.com>
2020-10-09 20:43:52 -0300

    fixed detekt undocumented turn commerce in data class and turn company private since it is a delegator

--- src/main/kotlin/dev/fakek/fakes/FakeCommerce.kt
+++ src/main/kotlin/dev/fakek/fakes/FakeCommerce.kt
@@ -5,4 +5,4 @@ import dev.fakek.interfaces.Company
 /**
  * FakeCommerce provides a commerce with random information.
  */
-class FakeCommerce(val address: FakeAddress, fakeCompany: FakeCompany) : Company by fakeCompany
+data class FakeCommerce(val address: FakeAddress, private val fakeCompany: FakeCompany) : Company by fakeCompany
