5549177fd24cf28e9f59472a99c38ccebe25d165
RafaelPereiraSantos <rafaelps999@gmail.com>
2020-10-13 21:05:52 -0300

    add empty line

--- src/main/kotlin/dev/fakek/fakes/FakeCrypto.kt
+++ src/main/kotlin/dev/fakek/fakes/FakeCrypto.kt
@@ -3,10 +3,14 @@ package dev.fakek.fakes
 /**
  * FakeCrypto provides a instance with fake generated cryptography.
  *
- * @param md5 message digested in a 128 bit value, see more in [here](https://en.wikipedia.org/wiki/MD5)
- * @param sha1 a random value in cryptography in secure hash algorithm 1, see more in [here](https://en.wikipedia.org/wiki/SHA-1)
- * @param sha256 a random value encrypted in secure hash algorithm 2 of 256 family, see more in [here](https://en.wikipedia.org/wiki/SHA-2)
- * @param sha512 a random value encrypted in secure hash algorithm 2 of 512 family, see more in [here](https://en.wikipedia.org/wiki/SHA-2)
+ * @param md5 message digested in a 128 bit value,
+ * see more in [here](https://en.wikipedia.org/wiki/MD5)
+ * @param sha1 a random value in cryptography in secure hash algorithm 1,
+ * see more in [here](https://en.wikipedia.org/wiki/SHA-1)
+ * @param sha256 a random value encrypted in secure hash algorithm 2 of 256 family,
+ * see more in [here](https://en.wikipedia.org/wiki/SHA-2)
+ * @param sha512 a random value encrypted in secure hash algorithm 2 of 512 family,
+ * see more in [here](https://en.wikipedia.org/wiki/SHA-2)
  */
 data class FakeCrypto(
     val md5: String,
